17be47907a569f0e2a3fe04a43373bff3fece40e
TeaMitic <tea.mitic20@gmail.com>
2022-11-02 00:16:17 +0100

    Register init

---
+++ app/src/main/java/elfak/mosis/tourguide/ui/screens/RegisterScreen.kt
@@ -0,0 +1,16 @@
+package elfak.mosis.tourguide.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RegisterScreen() {
+    Text("Register page")
+}
+
+@Preview
+@Composable
+fun PreviewRegister(){
+    Text("Register page")
+}
