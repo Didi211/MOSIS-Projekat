8e16a6d227247bc3d9d7b7d4232004dea1f8e42f
Didi211 <miticd99@gmail.com>
2023-01-11 00:13:08 +0100

    optimize imports

--- app/src/main/java/elfak/mosis/tourguide/MainActivity.kt
+++ app/src/main/java/elfak/mosis/tourguide/MainActivity.kt
@@ -2,15 +2,12 @@ package elfak.mosis.tourguide
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import elfak.mosis.tourguide.ui.navigation.Navigation
 import elfak.mosis.tourguide.ui.theme.TourGuideTheme
